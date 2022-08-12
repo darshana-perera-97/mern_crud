90514b270921047526bf1985e5db108fda6a4bf7
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-12 09:01:59 +0530

    adding new ui design created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import axios from "axios";
 
 function App() {
+  const [item, setItem] = React.useState({ title: "", description: "" });
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setItem((prevInput) => {
+      return {
+        ...prevInput,
+        [name]: value,
+      };
+    });
+    console.log(item);
+  }
+  function addItem(event) {
+    event.preventDefault();
+    const newItem = { title: item.title, description: item.description };
+    axios.post("/newitem", newItem);
+    console.log(newItem);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onChange={handleChange}
+        name="title"
+        placeholder="title"
+        value={item.title}
+      ></input>
+      <input
+        onChange={handleChange}
+        name="description"
+        placeholder="title"
+        value={item.description}
+      ></input>
+      <button onClick={addItem}>Add Item</button>
     </div>
   );
 }
