9dae746b16fd7bb87305630c65e45f975b0dcbcf
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-12 08:09:30 +0530

    server created in server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const port = 3001;
+
+//cpnnect to mongodb
+
+//data schema
+
+//data model
+
+//read route
+
+//create route
+
+//delete route
+
+//update route
+
+app.listen(port, function () {
+  console.log("Express is running");
+});
