1ca8b8fa7e132ac1fa3e19a67bd729872aafc753
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-12 14:39:08 +0530

    nodemon installed

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const port = 3001;
 
+//config
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //cpnnect to mongodb
 mongoose.connect(
   "mongodb+srv://darshana:darshana@cluster0.uoooe.mongodb.net/items?retryWrites=true&w=majority"
